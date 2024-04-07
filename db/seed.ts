f424f516a390dc43c7ac0afa54bd23b25aca16a5
Israni, Murli <murli.p.israni@gmail.com>
2024-04-07 05:32:36 +0530

    misc

--- db/seed.ts
+++ db/seed.ts
@@ -2,8 +2,12 @@ import { db, WaitingList } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  await db.insert(WaitingList).values([
-    { email: "johndoe@example.com" },
-    { email: "janedoe@example.com" },
-  ]);
+  await db
+    .insert(WaitingList)
+    .values([
+      { email: "johndoe@example.com" },
+      { email: "impondesk@gmail.com" },
+    ]);
 }
+
+///astro dev --port $PORT
